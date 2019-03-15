edfb360f1b19799d6728a586bcb70021de14cab2
Latif Hidayatullah <latifdayatofficial@gmail.com>
2019-03-15 08:31:07 +0700

    Grouping routes

--- routes/web.php
+++ routes/web.php
@@ -22,42 +22,71 @@ Route::get('designs/{design}', 'ClientController@show')->name('design.show');
 # Designer
 Auth::routes();
 
-Route::get('/home', 'DesignerController@home')->name('home');
-Route::get('designs', 'DesignerController@index')->name('design.index');
-Route::get('profile', 'DesignerController@getUserDetails')->name('user.show');
-Route::post('designs/{design}', 'DesignerController@updateTake')->name('design.update.take');
-Route::put('designs/{design}', 'DesignerController@updateDrop')->name('design.update.drop');
-Route::patch('designs/{design}', 'DesignerController@updateFinish')->name('design.update.finish');
+Route::group(["prefix" => "designer", "name.designer."], function() {
+    Route::get('/dashboard', 'DesignerController@home')->name('home');
+    Route::get('profile', 'DesignerController@getUserDetails')->name('user.show');
+
+    Route::group(["prefix" => "designs", "name" => "designer.design."], function() {
+        Route::get('/', 'DesignerController@index')->name('index');
+        Route::post('/{design}', 'DesignerController@takeOrder')->name('take');
+        Route::put('/{design}', 'DesignerController@dropOrder')->name('drop');
+        Route::patch('/{design}', 'DesignerController@orderFinished')->name('finish');
+    });
+});
+
+// AdminAuth
+Route::group(["prefix" => "admin", "namespace" => "AdminAuth"], function() {
+
+    Route::get('login', 'LoginController@showLoginForm')->name('admin.login');
+    Route::post('login', 'LoginController@login')->name('admin.login');
+    Route::post('/logout', 'LoginController@logout')->name('admin.logout');
+
+    Route::get('register', 'RegisterController@showRegistrationForm')->name('admin.register');
+    Route::post('register', 'RegisterController@register')->name('admin.register');
+
+    Route::group(["prefix" => "password"], function () {
+      // url: /password/...
+        Route::get('/reset', 'ForgotPasswordController@showLinkRequestForm')->name('admin.password.request');
+        Route::get('/reset/{token}', 'ResetPasswordController@showResetForm')->name('admin.password.reset');
+        Route::post('/reset', 'ResetPasswordController@reset')->name('admin.password.update');
+        Route::post('email', 'ForgotPasswordController@sendResetLinkEmail')->name('admin.password.email');
+    });
+});
 
 # Admin
-Route::group(['prefix' => 'admin'], function() {
-	Route::get('login', 'AdminAuth\LoginController@showLoginForm')->name('admin.login');
-	Route::post('login', 'AdminAuth\LoginController@login')->name('admin.login');
-	Route::get('/', 'AdminController@index')->name('admin.home');
-    Route::post('/logout', 'AdminAuth\LoginController@logout')->name('admin.logout');
-    Route::get('password/reset', 'AdminAuth\ForgotPasswordController@showLinkRequestForm')->name('admin.password.request');
-    Route::post('password/email', 'AdminAuth\ForgotPasswordController@sendResetLinkEmail')->name('admin.password.email');
-    Route::get('password/reset/{token}', 'AdminAuth\ResetPasswordController@showResetForm')->name('admin.password.reset');
-    Route::post('password/reset', 'AdminAuth\ResetPasswordController@reset')->name('admin.password.update');
-    Route::get('register', 'AdminAuth\RegisterController@showRegistrationForm')->name('admin.register');
-    Route::post('register', 'AdminAuth\RegisterController@register')->name('admin.register');
-    Route::get('profile/{admin}', 'AdminController@getAdminDetails')->name('admin.show');
-    Route::get('edit/{admin}', 'AdminController@getEditAdmin')->name('admin.edit');
-    Route::post('profile/{admin}', 'AdminController@updateAdmin')->name('admin.update');
+Route::group(['prefix' => 'admin', "namespace" => "Admin"], function() {
+
+	  Route::get('/', 'AdminController@index')->name('admin.home');
+
+    Route::group(["prefix" => "profile"], function() {
+        $name = "admin";
+        Route::get('/{admin}', 'AdminController@show')->name("$name.show");
+        Route::get('/{admin}/edit', 'AdminController@edit')->name("$name.edit");
+        Route::post('/{admin}', 'AdminController@update')->name("$name.update");
+    });
 
     # Design Routes
-    Route::get('design', 'AdminController@getAllOpenDesigns')->name('admin.design.open');
-    Route::get('design/progress', 'AdminController@getAllInProgressDesigns')->name('admin.design.inprogress');
-    Route::get('design/finished', 'AdminController@getAllFinishedDesigns')->name('admin.design.finished');
+    Route::group(["prefix" => "designs"], function() {
+        $name = "admin.design";
+        Route::get('/', 'AdminDesignController@showOpen')->name("$name.open");
+        Route::get('/progress', 'AdminDesignController@showInProgress')->name("$name.in_progress");
+        Route::get('/finished', 'AdminDesignController@showFinished')->name("$name.finished");
+    });
 
     # User Routes
-    Route::get('users', 'AdminController@getAllActiveUsers')->name('admin.user.index');
-    Route::get('trashed-users', 'AdminController@getAllTrashedUsers')->name('admin.user.trashed');
-    Route::get('users/{user}', 'AdminController@getUserDetails')->name('admin.user.show');
-    Route::get('users/edit/{user}', 'AdminController@getEditUser')->name('admin.user.edit');
-    Route::post('users/{user}', 'AdminController@updateUser')->name('admin.user.update');
-    Route::delete('users/{user}', 'AdminController@deleteUser')->name('admin.user.delete');
-    Route::put('users/{id}', 'AdminController@restoreUser')->name('admin.user.restore');
-    Route::delete('users/force/{id}', 'AdminController@destroyUser')->name('admin.user.destroy');
+    Route::get('users', 'AdminUserController@index')->name('admin.user.index');
+    Route::get('users/banned', 'AdminUserController@banned')->name('admin.user.trashed');
+
+    # User Management for Admin
+    Route::group(['prefix' => 'users/{user}', "name" => ""], function() {
+        $name = "admin.user";
+        Route::get('/', 'AdminUserController@show')->name("$name.show");
+        Route::get('/edit', 'AdminUserController@edit')->name("$name.edit");
+        Route::post('/', 'AdminUserController@store')->name("$name.update");
+        Route::delete('/', 'AdminUserController@delete')->name("$name.delete");
+        Route::put('/', 'AdminUserController@restore')->name("$name.restore");
+        Route::delete('/delete', 'AdminUserController@destroy')->name("$name.destroy");
+    });
+
 
 });
