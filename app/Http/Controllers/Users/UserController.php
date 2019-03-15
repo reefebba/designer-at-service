19a12bd361aebc21af868bb1f9584382664784f1
Latif Hidayatullah <latifdayatofficial@gmail.com>
2019-03-15 08:30:33 +0700

    Preparing folder for migrating UserController.php in Controllers folder

---
+++ app/Http/Controllers/Users/UserController.php
@@ -0,0 +1,11 @@
+<?php
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+
+class UserController extends Controller
+{
+    protected $view_dir = "user/design";
+
+    public function 
+}
