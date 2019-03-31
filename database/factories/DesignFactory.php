8cbaaac7fbcc1a93815dab0d41ec368345824a82
reefebba <rifebba@gmail.com>
2019-03-31 14:44:20 +0700

    seed and factories

--- database/factories/DesignFactory.php
+++ database/factories/DesignFactory.php
@@ -7,7 +7,9 @@ $factory->define(Design::class, function (Faker $faker) {
 	$designer = factory(App\Models\Designer::class)->create();
     return [
     	'designer_id' => $designer->id,
+    	'status' => 'in-progress',
         'size' => 'A4',
+        'image' => $faker->imageUrl,
         'add_info' => 'Nullable',
     ];
 });
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Models\Designer;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,6 +15,12 @@ class DatabaseSeeder extends Seeder
         // $this->call(UsersTableSeeder::class);
         factory(App\Models\Client::class, 9)->create();
         factory(App\Models\Lecture::class, 9)->create();
-        factory(App\Models\Manager::class)->create();
+        $manager = new Designer;
+        $manager->name = 'PM';
+        $manager->email = 'p@m.com';
+        $manager->email_verified_at = now();
+        $manager->password = 'MultimedProgram';
+        $manager->can = 'manage';
+        $manager->save();
     }
 }
