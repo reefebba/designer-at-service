9989a4de97638616eac42957a497582e33a9c15d
akromsofwan <48788955+akromsofwan@users.noreply.github.com>
2019-05-04 06:16:19 +0700

    add view manager profile design (#13)
    * commit 1
    
    * comit 2
    
    * manager views and controller
    
    * resolving conflicts
    
    * need further refinement too
    
    * manager and designer dashboard
    
    * before alpha release 2
    
    * change appearance for clients

--- resources/views/client/component/formcheck.blade.php
+++ resources/views/client/component/formcheck.blade.php
@@ -1,29 +1,19 @@
- {{-- Form: Check Status, Redirecting --}}
-<div class="row justify-content-center">
-    <div class="col-5 col-md-6">
-
-      <div class="form-group">
-        
-        <div class="card" style="width: 35rem;">
-          <h5 class="card-header info-color text-center">
-          <strong>Check Status</strong>
-          </h5>
-
-          <div class="card-body px-lg-5 pt-0">
-            <form class="border border-light p-5" method="POST" action="{{ route('client.design.check') }}" enctype="multipart/form-data">
-
-              @csrf
-
-              <p class="h4 mb-4 text-center">Masukan code unik design poster Anda</p>
-
-              <input type="text" name="uuid" class="form-control mb-4" placeholder="64baa976-c689-41e7-bdd1-0da5edad9712" required>
-              <div class="form-group text-center">
-                <button class="btn btn-info my-4" type="submit">Ok, Check!</button>        
-              </div>
-            </form>
+<div class="formcheck mt-3">
+  <div class="col-md-10 offset-md-1">
+    <div class="card">
+      <h5 class="card-header info-color text-center">
+        <strong>Check Status</strong>
+      </h5>
+      <div class="card-body  mt-3">
+        <form class="border border-light p-5" method="POST" action="{{ route('client.design.check') }}" enctype="multipart/form-data">
+          @csrf
+          <p class="h4 mb-4 text-center">Masukan code unik design poster yang anda pesan</p>
+          <input type="text" name="uuid" class="form-control mb-4" placeholder="64baa976-c689-41e7-bdd1-0da5edad9712" autocomplete="off" >
+          <div class="form-group text-center">
+            <button class="btn btn-info my-4" type="submit">Ok, Check!</button>        
           </div>
-        </div>
+        </form>
       </div>
     </div>
+  </div>
 </div>
-{{-- End Form: Check Status, Redirecting --}}
